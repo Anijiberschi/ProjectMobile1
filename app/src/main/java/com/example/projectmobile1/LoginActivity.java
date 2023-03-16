2a00f03ad1fa540c0567005f9941a1a088334302
arthur <arthur.nijiberschi@helb-prigogine.be>
2023-03-16 16:15:50 +0100

    Ajout de la partie login

--- app/src/main/java/com/example/projectmobile1/LoginActivity.java
+++ app/src/main/java/com/example/projectmobile1/LoginActivity.java
@@ -1,14 +1,74 @@
 package com.example.projectmobile1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.badge.BadgeUtils;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email, password;
+    private Button login;
+    private  FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.et_emailLoginActivity);
+        password = findViewById(R.id.et_passwordLoginActivity);
+        login = findViewById(R.id.bt_loginLoginActivity);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String log_email = email.getText().toString();
+                String log_password = password.getText().toString();
+
+                mAuth.signInWithEmailAndPassword(log_email, log_password)
+                        .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(LoginActivity.this, "Authentication success.",Toast.LENGTH_SHORT).show();
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    finish();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(LoginActivity.this, "Authentication failed.",Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+            }
+        });
+
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Toast.makeText(LoginActivity.this, "You're already connected", Toast.LENGTH_SHORT ).show();
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/projectmobile1/User.java
+++ app/src/main/java/com/example/projectmobile1/User.java
@@ -1,6 +1,8 @@
 package com.example.projectmobile1;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
     private String name, email, description;
 
     public User(){}
@@ -8,6 +10,31 @@ public class User {
     public User(String name, String email, String description){
         this.name= name;
         this.email = email;
+        this.description = description;
+    }
+
+    // getters and setters for the private fields
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
         this.description = description;
     }
 }
